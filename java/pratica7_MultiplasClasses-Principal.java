4bdcef5f84e1d51b60b353e6d6f870d12d516b77
Beatriz Miranda <49699735+beatrizadm@users.noreply.github.com>
2020-05-25 17:38:10 -0300

    Update and rename Principal.java to java/pratica7_MultiplasClasses-Principal.java

--- java/pratica7_MultiplasClasses-Principal.java
+++ java/pratica7_MultiplasClasses-Principal.java
@@ -2,7 +2,7 @@ package pratica7_multiplasclasses;
 
 public class Principal {
 	public static void main(String[] args) {
-		System.out.println("Vai começar a contagem:");
+		System.out.println("Vai comecar a contagem:");
 		System.out.println(Operacoes.contador(1,5));
 	}
 }
